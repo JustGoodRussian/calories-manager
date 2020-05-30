be23bacf464aadb484ff7cf33366b283c1e5c5d7
Andrey Dil <siberia.novosibirsk.dil@gmail.com>
2020-05-30 21:41:25 +0700

    Adding logging to the project

--- src/main/java/com/dil/calories/web/MealServlet.java
+++ src/main/java/com/dil/calories/web/MealServlet.java
@@ -1,5 +1,9 @@
 package com.dil.calories.web;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+import javax.servlet.ServletException;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
@@ -7,10 +11,11 @@ import java.io.IOException;
 
 public class MealServlet extends HttpServlet {
 
+    private static final Logger LOGGER = LoggerFactory.getLogger(MealServlet.class);
+
     @Override
-    protected void doGet(HttpServletRequest request, HttpServletResponse response) throws IOException {
-
-        response.sendRedirect("meals.jsp");
-
+    protected void doGet(HttpServletRequest request, HttpServletResponse response) throws IOException, ServletException {
+        LOGGER.debug("forward to meals");
+        request.getRequestDispatcher("/meals.jsp").forward(request, response);
     }
 }
--- src/main/java/com/dil/calories/web/UserServlet.java
+++ src/main/java/com/dil/calories/web/UserServlet.java
@@ -1,5 +1,9 @@
 package com.dil.calories.web;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+import javax.servlet.ServletException;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
@@ -7,10 +11,11 @@ import java.io.IOException;
 
 public class UserServlet extends HttpServlet {
 
+    private static final Logger LOGGER = LoggerFactory.getLogger(UserServlet.class);
+
     @Override
-    protected void doGet(HttpServletRequest request, HttpServletResponse response) throws IOException {
-
-        response.sendRedirect("users.jsp");
-
+    protected void doGet(HttpServletRequest request, HttpServletResponse response) throws IOException, ServletException {
+        LOGGER.debug("forward to users");
+        request.getRequestDispatcher("/users.jsp").forward(request, response);
     }
 }
